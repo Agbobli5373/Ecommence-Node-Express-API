b5f42380df99e02363a4d305983154d336c40362
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-01-11 18:57:53 +0000

    Adding user route and models Commit

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/UserModel");
+
 //POST route
 router.post("/signup", (req, res, next) => {
   User.find({ email: req.body.email })
@@ -9,35 +10,37 @@ router.post("/signup", (req, res, next) => {
     .then((user) => {
       if (user.length >= 1) {
         return res.status(409).json({
-          message: "User already exit",
+          message: "User Email already exit",
         });
       } else {
-        bcrypt
-          .hash(req.body.password, 10, (err, hash) => {
-            if (err) {
-              return res.status(500).json({
-                Error: err,
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              Error: err,
+            });
+          } else {
+            const user = new User({
+              email: req.body.email,
+              password: hash,
+            });
+            user.save().then((result) => {
+              res.status(201).json({
+                message: "User Created",
+                USER :{
+                    Email : result.email,
+                    Password : result.password
+                }
               });
-            } else {
-              const user = new User({
-                email: req.body.email,
-                password: hash,
-              });
-              user.save().then((result) => {
-                res.status(201).json({
-                  message: "User Created",
-                  result,
-                });
-              });
-            }
-          })
+            });
+          }
+        });
       }
     })
     .catch((err) => {
-        res.status(500).json({
-          Error: err,
-        });
+      res.status(500).json({
+        Error: err,
       });
+    });
 });
 
 //POST route
@@ -46,8 +49,43 @@ router.post("/", (req, res, next) => {
 });
 
 //DELETE route
-router.delete("/", (req, res, next) => {
-  res.status(201).json({ message: "Delete User Route" });
+router.delete("/:email", (req, res, next) => {
+    const id = req.body.userId;   
+    User.remove({email:req.body.email})
+        .exec()
+        .then((resut)=>{
+            res.status(200).json({
+              messgae : "User is Deleted successful"
+            })
+        })
+        .catch((err)=>{
+            res.status(500).json({
+                Error : err
+            })
+        })
 });
+
+router.get('/',(req,res,next)=>{
+    User.find({})
+        .exec()
+        .then((result)=>{
+            res.status(200).json({
+                User : result.map((user)=>{
+                    return (
+                        {
+                            _id : user._id,
+                            email : user.email
+                        }
+                    )
+                })
+            })
+           
+        })
+        .catch((err)=>{
+            res.status(500).json({
+                Error : err
+            })
+        })
+})
 
 module.exports = router;
